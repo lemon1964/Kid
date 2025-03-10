8c010773219375f00ec491274b848912392b761d
lemon <lemon@example.com>
2025-03-10 03:14:40 +0300

    github-deploy

--- backend/settings.py
+++ backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
-# Определение окружения (local или production)
+# Определение окружения (local или production)н
 DJANGO_ENV = config('DJANGO_ENV')
 
 if DJANGO_ENV == 'local':
