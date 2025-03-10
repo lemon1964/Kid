9efc482eaf8c4d81aa43d2caf9d9dc4480aa0b33
lemon <lemon@example.com>
2025-03-11 00:44:00 +0300

    psycopg2-binary

--- backend/settings.py
+++ backend/settings.py
@@ -54,6 +54,9 @@ else:
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
     
