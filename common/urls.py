d90252de443e957374c0721ba69221a809c3ab25
lemon <lemon@example.com>
2025-06-10 01:59:59 +0300

    no code

--- common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_task_by_id, get_task_list
-
-urlpatterns = [
-    path("list/", get_task_list, name="get_task_list"),
-    path("<str:unique_id>/", get_task_by_id, name="get_task_by_id"),
-]
